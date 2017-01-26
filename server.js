885a9ef03a035dcb2f9b73074ad11df8d5b7fee4
j-pai <jesse.pai@gmail.com>
2017-01-26 16:03:35 -0500

    Added error page handling

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 var express = require("express");
 var app = express();
 
-var router = express.Router();
-
 // Necessary to allow environment to set PORT
 app.set('port', (process.env.PORT || 80));
 
 app.set('view engine', 'ejs');
 
-router.get('/', function(req, res) {
+app.get('/', function(req, res) {
     res.render("pages/index");
 });
 
-router.get('/about', function(req,res) {
+app.get('/about', function(req, res) {
     res.render('pages/about');
 });
 
-app.use("*", function(req,res) {
-    res.sendFile("pages/404");
+// Page not found error
+app.get('/404', function(req, res, next){
+    // Trigger a 404
+    next();
+});
+
+app.use(function(req, res, next) {
+    res.status(404);
+    res.render('pages/404', {url: req.url});
+    return;
 });
 
 app.listen(app.get('port'), function() {
